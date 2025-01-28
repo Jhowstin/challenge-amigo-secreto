83d90dcf6499ff226d9d717e7697badcef278cd5
Jhowstin Hernandez <jhowstin1129@gmail.com>
2025-01-27 20:55:00 -0400

    agregando funcion para sortear los amigos almacenados en la array

--- app.js
+++ app.js
@@ -21,15 +21,34 @@ function agregarAmigo() {
 }
 
 function actualizarListaDeAmigos() {
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
-    nombresDeAmigos.forEach(function(nombre) {
-    // Crear un nuevo elemento <li>
-    let li = document.createElement("li");
-    // Establecer el contenido de texto del <li> al nombre actual
-    li.textContent = nombre;
-      
-    // Agregar el <li> a la lista HTML
-    lista.appendChild(li);
-    });
+  // Obtener el elemento de la lista de amigos por su ID
+  let lista = document.getElementById("listaAmigos");
+  // Limpiar la lista antes de agregar nuevos elementos
+  lista.innerHTML = "";
+  // Iterar sobre cada nombre en la lista de nombres de amigos
+  nombresDeAmigos.forEach(function(nombre) {
+      // Crear un nuevo elemento <li> para cada nombre
+      let li = document.createElement("li");
+      // Establecer el contenido de texto del <li> al nombre actual
+      li.textContent = nombre;
+      // Agregar el <li> a la lista HTML
+      lista.appendChild(li);
+  });
+}
+
+// Función para seleccionar un amigo aleatorio de la lista
+function sortearAmigo() {
+  // Verificar si la lista de amigos está vacía
+  if (nombresDeAmigos.length === 0) {
+    alert("No hay amigos en la lista para seleccionar.");
+    return;
+  } 
+  // Obtener un índice aleatorio
+  let indiceAleatorio = Math.floor(Math.random() * nombresDeAmigos.length);
+  
+  // Seleccionar el nombre correspondiente al índice aleatorio
+  let amigoAleatorio = nombresDeAmigos[indiceAleatorio];
+  
+  // Mostrar el nombre del amigo seleccionado
+document.getElementById("resultado").innerHTML = "Amigo seleccionado: " + amigoAleatorio;
 }
