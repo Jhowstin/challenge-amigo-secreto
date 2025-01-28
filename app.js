febcbad6ca6e593fde769b7461221d2578f06151
Jhowstin Hernandez <jhowstin1129@gmail.com>
2025-01-27 20:24:42 -0400

    git status

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Declarar un array vacío para almacenar los nombres de los amigos
+let nombresDeAmigos = [];
+
+// Definir la función para agregar un amigo
+function agregarAmigo() {
+  // Obtener el valor del campo de entrada con id "amigo"
+  let nombre = document.getElementById("amigo").value;
+  
+  // Verificar si el campo de entrada está vacío
+  if (nombre == '') {
+    // Mostrar una alerta si el campo está vacío
+    alert("No se puede agregar un amigo sin nombre");
+  } else {
+    // Agregar el nombre al array de amigos
+    nombresDeAmigos.push(nombre);
+    // Limpiar el campo de entrada
+    document.getElementById("amigo").value = "";
+
+  }
+}
